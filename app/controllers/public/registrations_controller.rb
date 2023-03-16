5dd28220053a8f1f2a1b09324ddfb9747e30f852
TakaoKizu <117328822+takalong3812@users.noreply.github.com>
2023-03-16 01:41:30 +0000

    bootstrap ok

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
+  def after_sign_up_path_for(resource)
+  flash[:notice]="会員登録完了"
+  public_user_path(resouce)
+  end  
 end
